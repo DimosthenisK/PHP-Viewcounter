994d57f710f685a233df5316e68d218f1fa92bac
voidblaze <ftv.voidblaze@gmail.com>
2018-01-26 13:57:42 +0200

    added settings & method for addView

---
+++ viewcounter/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_NAME = "viewcounter";
+const DB_PASS = "";
+
+const USER_PASS = "";
+const EXPIRATION = 60 * 60 * 24 * 30 * 12;
+
+const debug = true;
+?>
